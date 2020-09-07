411669e8ff278fbb55a086bb0a9c6bac67c69b96
vjajvjjain <vj98965@gmail.com>
2020-09-07 20:12:09 +0530

    ready for deployment

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,10 @@ import { createStore, applyMiddleware } from "redux";
 import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 import NewRootReducer from "./root-reducer";
-const middlewares = [logger];
+const middlewares = [];
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
 const store = createStore(NewRootReducer, applyMiddleware(...middlewares));
 const persestor = persistStore(store);
 export { store, persestor };
